bb57a135c93b316a46af945e2349327a6276f4bf
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-04 03:01:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-   katz_deli.length == 0
+  katz_deli.length == 0
     puts "There is nobody waiting to be served!"
+  katz_deli.
 end
