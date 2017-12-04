3e8bdfe37230883398ee9826308f947b6e854d6a
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-04 02:32:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else katz_deli
-    puts %w("Currently serving Logan.")
+    puts %w << ("Currently serving Logan.")
   end
 end
