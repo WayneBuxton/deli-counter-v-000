508854b610cb6aa06e3b59a5d1def64b2c5a806d
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-04 01:58:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,7 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli << name
-    if katz_deli.each_with_index do |name,index|
-      puts "Welcome, #{name}. You are number #{index + 1} in line."
-    end
-  end
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
